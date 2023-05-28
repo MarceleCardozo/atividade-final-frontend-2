b87528bba53b8aec77df89c2ff347bbf4fceffc9
MarceleDutra Cardozo <marcelecardozo@gmail.com>
2023-05-28 20:52:42 -0300

    Adicionando a funcionalidade que busca na API do Rick and Morty o que foi digitado no campo de pesquisa

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const api = axios.create({
   baseURL: "https://rickandmortyapi.com/api",
 });
+
+//busca na API do Rick and Morty com base no valor inserido no campo de pesquisa.
+function search(event) {
+  event.preventDefault();
+
+  let searchBarValue = document.getElementById("search-bar").value;
+
+  api
+    .get("https://rickandmortyapi.com/api/character", {
+      params: { name: searchBarValue },
+    })
+    .then((response) => {
+      const filteredCharacters = response.data.results.filter((character) => {
+        return character.name
+          .toLowerCase()
+          .includes(searchBarValue.toLowerCase());
+      });
+
+      console.log(filteredCharacters);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
