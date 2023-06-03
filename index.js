b26dba92595add9d113a836a94a75dfeebfb8712
MarceleDutra Cardozo <marcelecardozo@gmail.com>
2023-06-03 15:45:49 -0300

    inserindo o nome do episódio que cada personagem esteve por último

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let currentPage = 1;
 let totalPages = 1;
 
 // Search in the Rick and Morty API based on the value entered in the search bar.
-function search(event) {
+async function search(event) {
   event.preventDefault();
 
   let searchBarValue = document.getElementById("search-bar").value;
@@ -20,62 +20,63 @@ function search(event) {
     params.name = searchBarValue;
   }
 
-  api
-    .get(apiUrl, { params })
-    .then((response) => {
-      const filteredCharacters = response.data.results;
-      let cards = document.querySelector(".containerCards");
-      cards.innerHTML = "";
+  try {
+    const response = await api.get(apiUrl, { params });
+    const filteredCharacters = response.data.results;
+    let cards = document.querySelector(".containerCards");
+    cards.innerHTML = "";
 
-      filteredCharacters.forEach((character, index) => {
-        let statusColorClass = ""; // Declare the statusColorClass variable
+    for (const character of filteredCharacters) {
+      const episodeNResponse = await axios.get(character.episode[0]);
+      const episodeName = episodeNResponse.data.name;
 
-        if (character.status === "Alive") {
-          statusColorClass = "green-status";
-        } else if (character.status === "Dead") {
-          statusColorClass = "red-status";
-        } else {
-          statusColorClass = "gray-status";
-        }
+      let statusColorClass = ""; // Declare the statusColorClass variable
 
-        cards.innerHTML += `
-    <div class="card">
-      <img src="https://rickandmortyapi.com/api/character/avatar/${character.id}.jpeg"/>
-      <div class="informations">
-        <span class="title">${character.name}</span> 
-        <div class="statusCharacter">
-          <div class="statusColor ${statusColorClass}"></div>
-          <div class="statusText">${character.status} - ${character.species}</div>
-        </div>
-        <div class="lastCharaterLocation">
-          <p>Last known location:</p>
-          <span>${character.location.name}</span>
-        </div>
-        <div>
-          <p>Last seen on:</p>
-          <span>${character.episode.name}</span>
-        </div>
-      </div>
-    </div>
-  `;
+      if (character.status === "Alive") {
+        statusColorClass = "green-status";
+      } else if (character.status === "Dead") {
+        statusColorClass = "red-status";
+      } else {
+        statusColorClass = "gray-status";
+      }
 
-        if ((index + 1) % 2 === 0 && index !== filteredCharacters.length - 1) {
-          cards.innerHTML += `<hr class="greenHr"/>`;
-        }
-      });
+      cards.innerHTML += `
+        <div class="card">
+          <img src="https://rickandmortyapi.com/api/character/avatar/${character.id}.jpeg"/>
+          <div class="informations">
+            <span class="title">${character.name}</span> 
+            <div class="statusCharacter">
+              <div class="statusColor ${statusColorClass}"></div>
+              <div class="statusText">${character.status} - ${character.species}</div>
+            </div>
+            <div class="lastCharaterLocation">
+              <p>Last known location:</p>
+              <span>${character.location.name}</span>
+            </div>
+            <div>
+              <p>Last seen on:</p>
+              <span>${episodeName}</span>
+            </div>
+          </div>
+        </div>`;
 
-      console.log(filteredCharacters);
+      if (
+        (filteredCharacters.indexOf(character) + 1) % 2 === 0 &&
+        filteredCharacters.indexOf(character) !== filteredCharacters.length - 1
+      ) {
+        cards.innerHTML += `<hr class="greenHr"/>`;
+      }
+    }
 
-      // Update pagination information
-      const pagination = response.data.info;
-      totalPages = pagination.pages;
+    // Update pagination information
+    const pagination = response.data.info;
+    totalPages = pagination.pages;
 
-      // Add pagination buttons
-      addPaginationButtons();
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    // Add pagination buttons
+    addPaginationButtons();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function addPaginationButtons() {
